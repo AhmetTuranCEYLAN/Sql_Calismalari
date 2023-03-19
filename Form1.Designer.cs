78925160807e6d8d67130e6bbea0d3a528fc5288
AhmetTuranCEYLAN <ahmetturanceylan996@gmail.com>
2023-03-19 23:25:09 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace Sql_Calismalari
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(275, 160);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Okut 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DarkOrange;
+            this.label1.Font = new System.Drawing.Font("Segoe Script", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(239, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(342, 48);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Veri Tabanı İşlemleri";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(509, 160);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(163, 183);
+            this.treeView1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(390, 160);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 50);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Okut 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(275, 250);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(87, 50);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Okut 3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(390, 250);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(87, 50);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Okut 4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(275, 340);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(87, 50);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Okut 5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(390, 340);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(87, 50);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Okut 6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(124, 396);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(574, 137);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(70, 160);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(187, 147);
+            this.listBox1.TabIndex = 11;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(709, 160);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gold;
+            this.ClientSize = new System.Drawing.Size(884, 545);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sql_Calismalari
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\toshıba\Desktop\Görsel Programlama Çalışmalarım\Sql Çalışmaları\Sql_Calismalari\Ticari1.mdf;Integrated Security=True");
+            con.Open();
+
+            SqlCommand man = new SqlCommand("select * from department ", con);
+            SqlDataReader sr = man.ExecuteReader();
+            
+            while (sr.Read())
+            {
+
+                listBox1.Items.Add(sr["id"].ToString() + "  " + sr["name"].ToString() + "  " + sr["description"].ToString());
+
+                TreeNode tn = treeView1.Nodes.Add(sr["id"].ToString());
+                TreeNode tr= tn.Nodes.Add(sr["name"].ToString());
+                tr.Nodes.Add(sr["description"].ToString());
+            }
+            
+            sr.Close();
+        }
+
+
+      
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string str = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\toshıba\Desktop\Görsel Programlama Çalışmalarım\Sql Çalışmaları\Sql_Calismalari\Ticari1.mdf;Integrated Security=True";
+            SqlConnection con = new SqlConnection(str);
+            con.Open();
+
+
+
+            SqlCommand cmd = new SqlCommand("select * from department", con);
+            SqlDataReader dr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+            dataGridView1.DataSource = dt;
+
+           
+        }
+
+       
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string str = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\toshıba\Desktop\Görsel Programlama Çalışmalarım\Sql Çalışmaları\Sql_Calismalari\Ticari1.mdf;Integrated Security=True";
+            SqlConnection con = new SqlConnection(str);
+            con.Open();
+
+
+
+            SqlCommand cmd = new SqlCommand("select id, name from department where description like 'Er%'", con);
+            
+            SqlDataReader dr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+            dataGridView1.DataSource = dt;
+        }
+
+
+      
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\toshıba\Desktop\Görsel Programlama Çalışmalarım\Sql Çalışmaları\Sql_Calismalari\Ticari1.mdf;Integrated Security=True");
+            con.Open();
+
+            SqlCommand com = new SqlCommand("select name from department where name like 'V%' ", con);
+            SqlDataReader sr= com.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(sr);
+            listBox1.DataSource = dt;
+            listBox1.DisplayMember = "Name"; 
+            
+            
+
+        }
+
+        
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ColorDialog c = new ColorDialog();
+            
+            if (c.ShowDialog()==DialogResult.OK)
+            {
+                dataGridView1.ForeColor = c.Color;
+                treeView1.ForeColor = c.Color;
+                comboBox1.ForeColor = c.Color;
+            }
+        }
+
+
+        
+        private void button6_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\toshıba\Desktop\Görsel Programlama Çalışmalarım\Sql Çalışmaları\Sql_Calismalari\Ticari1.mdf;Integrated Security=True");
+            con.Open();
+            SqlCommand com = new SqlCommand("select name,description from department where name like 'V%'", con);
+            SqlDataReader dr = com.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+            comboBox1.DataSource = dt;
+            comboBox1.DisplayMember = "Description";
+            
+        }
+    }
+}
